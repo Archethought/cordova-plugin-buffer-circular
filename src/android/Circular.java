f72d19a7efd0552db4a93e704d8eefebdd33ac3b
Daniel P Williams <dwilliams@port8080.net>
2014-10-29 23:33:11 -0600

    Rewrote the code to the proper format for cordova.

--- src/android/Circular.java
+++ src/android/Circular.java
@@ -1,10 +1,11 @@
 /*** This is the Android Java Source File ***/
 package com.archethought.cordovaplugins.buffers;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaResourceApi;
-import org.apache.cordova.PluginResult;
+import org.apache.cordova.*;
+//import org.apache.cordova.CallbackContext;
+//import org.apache.cordova.CordovaPlugin;
+//import org.apache.cordova.CordovaResourceApi;
+//import org.apache.cordova.PluginResult;
 
 import android.content.Context;
 
@@ -23,14 +24,59 @@ public class Circular extends CordovaPlugin {
     private ArrayList<String> buffer; // String array for the circular buffer.
     private int max; // Maximum number of items to allow in the buffer.
     
-    /***
-     * Constructor.
-     ***/
-    public Circular() {
-        // This constructor defaults to 10 items.
-        // FIXME: Make this constructor default to 2/s * 60s * 30m items.
-        this.max = 10;
-        this.buffer = new ArrayList<String>( this.max);
+    /*** Cordova Magic Functions ***/
+    
+    @Override
+    public void initialize( final CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        
+        // Initialization of the goodies.
+        // Defaulting the buffer to 300.  May increase this later.
+        this.max = 300;
+        this.buffer = new ArrayList<String>( max)
+    }
+    
+    @Override
+    public boolean execute( String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        // These if statements call the actual functions.  There must be one for each public function.
+        // Push function
+        if( "push".equals( action)) {
+            this.push( args.getString( 0), callbackContext);
+            return true;
+        }
+        // Pop function
+        else if( "pop".equals( action)) {
+            this.pop( callbackContext);
+            return true;
+        }
+        // Peek function
+        else if( "peek".equals( action)) {
+            this.peek( callbackContext);
+            return true;
+        }
+        // Count function
+        else if( "count".equals( action)) {
+            this.count( callbackContext);
+            return true;
+        }
+        // Capacity function
+        else if( "capacity".equals( action)) {
+            this.capacity( callbackContext);
+            return true;
+        }
+        // Clear function
+        else if( "clear".equals( action)) {
+            this.clear( callbackContext);
+            return true;
+        }
+        // Set Capacity function
+        else if( "setCapacity".equals( action)) {
+            this.setCapacity( args.getInt( 0), callbackContext);
+            return true;
+        }
+        
+        // This is needed to be PC ( programmatically correct).
+        return false;
     }
     
     // Make another constructor here that allows a number of items to be set.
@@ -42,59 +88,80 @@ public class Circular extends CordovaPlugin {
      *   Remove the first item from the buffer if the buffer is full.
      *   Add an item to the end of the buffer.
      ***/
-    public void push( String newItem) {
+    public void push( String newItem, CallbackContext callbackContext) {
         // If full, remove the first item.
-        if( isFull()) {
+        if( this.isFull()) {
             this.buffer.remove( 0);
         }
         // Now add the new item to the end.
         this.buffer.add( newItem);
+        callbackContext.success();
         return;
     }
     
     /***
      * Pop function
      *   Return the item at the beginning of the buffer and remove it.
+     *   Value ( String) is returned using the callback context.
      ***/
-    public String pop() {
+    public void pop( CallbackContext callbackContext) {
         // Save the first item off the list.
         String temp = this.buffer[ 0];
         // Remove the first item.
         this.buffer.remove( 0);
         // Return that item.
-        return temp;
+        callbackContext.success( temp);
+        return;
     }
     
     /***
      * Peek function
      *   Return the item at the beginning of the buffer without removing it.
+     *   Value ( String) is returned using the callback context.
      ***/
-    public String peek() {
-        return this.buffer[ 0];
+    public void peek( CallbackContext callbackContext) {
+        callbackContext.success( this.buffer[ 0]);
+        return;
     }
     
     /***
      * Count function
      *   Return the number of item in the buffer.
+     *   Value ( int) is returned using the callback context.
      ***/
-    public int count() {
-        return this.buffer.size();
+    public void count( CallbackContext callbackContext) {
+        callbackContext.success( this.buffer.size());
+        return;
     }
     
     /***
-     * Max Size function
+     * Capacity function
      *   Return the number of items the buffer can hold.
+     *   Value ( int) is returned using the callback context.
      ***/
-    public int maxCount() {
-        return this.max;
+    public void capacity( CallbackContext callbackContext) {
+        callbackContext.success( this.max);
+        return;
     }
     
     /***
      * Clear function
      *   Reset the buffer to an empty state.
      ***/
-    public void clear() {
+    public void clear( CallbackContext callbackContext) {
         this.buffer = new ArrayList<String>( this.max);
+        callbackContext.success();
+        return;
+    }
+    
+    /***
+     * Set Capacity function
+     *   Change the number of items the buffer can hold.
+     ***/
+    public void setCapacity( int newCapacity, CallbackContext callbackContext) {
+        this.max = newCapacity;
+        this.buffer.censureCapacity( this.max);
+        callbackContext.success();
         return;
     }
     
--- www/buffer_circ.js
+++ www/buffer_circ.js
@@ -1,5 +1,87 @@
 /*** This is the javascript interface that the app uses. ***/
+//
+// Use jsdoc to generate documentation.
 
-module.exports = {
-    /*** Functions go here ***/
-}
+// The following line causes a jsdoc error.
+// Use the jsdoc option -l to ignore the error.
+var exec = cordova.require('cordova/exec');
+
+/** @module com.archethought.cordovaplugins.buffers.Circular */
+
+/** Push an item into of the buffer.
+* @param {string} string - A string that is to be pushed onto the buffer.
+* @example
+buffer_circ.push( string);
+*/
+exports.push = function( string) {
+    exec( null, null, 'Circular', 'push', [ string]);
+};
+
+/** Pop an item out of the buffer.
+* @param {resultCallback} resultCallback - The function that will handle the result.
+* @example
+buffer_circ.pop(
+    function( item) {
+        console.log('Popped item: ' + item);
+    }
+);
+*/
+exports.pop = function( resultCallback) {
+    exec( resultCallback, null, 'Circular', 'pop', []);
+};
+
+/** Peek at the next item out of the buffer.
+* @param {resultCallback} resultCallback - The function that will handle the result.
+* @example
+buffer_circ.peek(
+    function( item) {
+        console.log('Peeking at item: ' + item);
+    }
+);
+*/
+exports.peek = function( resultCallback) {
+    exec( resultCallback, null, 'Circular', 'peek', []);
+};
+
+/** Get the number of items in the buffer.
+* @param {resultCallback} resultCallback - The function that will handle the result.
+* @example
+buffer_circ.count(
+    function( number) {
+        console.log('Number of items: ' + item);
+    }
+);
+*/
+exports.count = function( resultCallback) {
+    exec( resultCallback, null, 'Circular', 'count', []);
+};
+
+/** Get the number of items the buffer can hold.
+* @param {resultCallback} resultCallback - The function that will handle the result.
+* @example
+buffer_circ.capacity(
+    function( number) {
+        console.log('Max number of items: ' + item);
+    }
+);
+*/
+exports.capacity = function( resultCallback) {
+    exec( resultCallback, null, 'Circular', 'capacity', []);
+};
+
+/** Clear the buffer out.
+* @example
+buffer_circ.clear();
+*/
+exports.clear = function() {
+    exec( null, null, 'Circular', 'clear', []);
+};
+
+/** Set the number of item the buffer can hold.
+* @param {number} maxCapacity - A string that is to be pushed onto the buffer.
+* @example
+buffer_circ.setCapacity( maxCapacity);
+*/
+exports.push = function( maxCapacity) {
+    exec( null, null, 'Circular', 'setCapacity', [ maxCapacity]);
+};
